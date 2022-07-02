60d65cc35f85b966ae1fe90548d1d2e3ca628b1c
egydeveloper <www.michael.m.s.com@gmail.com>
2022-07-02 22:58:00 +0200

    accessories

---
+++ Accesssiers Detailss/WindowsFormsApp1/accessContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class accessContext:DbContext
+    {
+        public accessContext() : base("name=mobilesdbs")
+        {
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Configure Student & StudentAddress entity
+            modelBuilder.Entity<accessierdetaile>()
+                        .HasOptional(s => s.Accessierimages) // Mark Address property optional in Student entity
+                        .WithRequired(ad => ad.Accessierdetaile);
+        }
+        public DbSet<accessierdetaile> accdetailes { get; set; }
+        public DbSet<accessierimages> accimages { get; set; }
+    
+    }
+}
